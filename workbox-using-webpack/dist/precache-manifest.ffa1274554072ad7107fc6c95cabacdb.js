6cdf6b18d20f9dca7e804f980bda82977059f1d0
csbun <icsbun@gmail.com>
2018-03-21 20:21:55 +0800

    build webpack

---
+++ workbox-using-webpack/dist/precache-manifest.ffa1274554072ad7107fc6c95cabacdb.js
@@ -0,0 +1,13 @@
+self.__precacheManifest = [
+  {
+    "revision": "93078ba73416973fc59b",
+    "url": "main.93078.css"
+  },
+  {
+    "revision": "93078ba73416973fc59b",
+    "url": "index.93078.js"
+  },
+  {
+    "url": "7186d37d76d392b0e8ad935d7829f6fb.png"
+  }
+];
